a10fd5d302dd858e5ad5f2ba952d6b8fcaadc13d
A M <rememberyourdreams@gmail.com>
2011-02-12 22:29:47 +0000

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Increment::Application.config.secret_token = '34f92469e4aace1e817c6e585f36cb985e58cf17c9983c0977a84cac5c165dde951dd970f5cfbc928f1daa0bb6dad775251e39d54eff5d8550daac47ae6e5cc9'
